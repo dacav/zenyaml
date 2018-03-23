b5ade3e598d634ccac79634f2ca21b6d9c4d55c0
Giovanni Simoni <giovanni.simoni@edgeware.tv>
2018-03-23 21:59:02 +0100

    Happy path test sketched
    Simple test, to start up the test suite

---
+++ tests/happypath.cc
@@ -0,0 +1,24 @@
+#include <zenyaml/parser.h>
+
+#include <cassert>
+#include <iostream>
+
+using namespace zenyaml;
+
+int main()
+{
+    char buffer[] =
+        "scalar: 1024\n"
+        "list:\n"
+        "  - foo\n"
+        "  - bar\n"
+        "map:\n"
+        "  key1: value1\n"
+        "  key2: value2\n"
+    ;
+
+    FILE* infile = fmemopen(buffer, sizeof(buffer) - 1, "r");
+
+    Parser parser(infile);
+    Node root = parser.get_document();
+}
